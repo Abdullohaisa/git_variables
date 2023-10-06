d1eba65d308aa6d8cc1642d07961c7695281a2d4
abdullohasia <tolaganova624@gmail.com>
2023-10-06 17:57:32 +0500

    first-commit

---
+++ main.js
@@ -0,0 +1,94 @@
+// begin_1 kvadrat perimetri
+// const a = parseInt(prompt("Kvadrat tomonini kiriting"));
+// const S = a * 4;
+// alert( S );
+
+// begin_2 kvadrat yuzasi
+// const a = parseInt(prompt("kvadrat tomonini kiriting"));
+// const S = a * a;
+// alert(S)
+
+// begin_3 tortburchakning yuzasi va perimetri
+// const a = parseInt(prompt("tortburchakning eni"));
+// const b = parseInt(prompt("tortburchakning bo'yi"));
+// const S = a * b ;
+// const P = 2 * ( a + b ) ; 
+// alert(S);
+// alert(P);
+
+// begin_4 Aylananing uzunligi aniqlansin 
+// const d = parseInt(prompt("diametrni kiriting"));
+// const pi = 3.14159265
+// const l = pi * d ;
+// alert(l);
+
+//begin_5 kub hajmi va yuzi 
+// const a = parseInt(prompt("kubning tomonini kiriting")) ;
+// const v = a * a * a ;
+// const s = a * a * 6 ;
+// alert(v);
+// alert(s);
+ 
+// begin_6 paralelipiped hajmi va yuzasi 
+// const a = parseInt(prompt("parelelipetning bo'yi kiriting"));
+// const b = parseInt(prompt("parelelipetning eni kiriting"));
+// const c = parseInt(prompt("parelelipetning balandligi kiriting"))
+// const v = a * b * c 
+// const s = 2 * (a * c + b * c + a * c );
+// alert(v);
+// alert(s);
+
+//  Begin_7 Doiraning uzunligi va yuzasi
+// const r = parseInt(prompt("Doiraning radiusini kiriting"));
+// const pi = 3.14159265;
+// const l = 2 * pi * r ;
+// const s = pi * r * r ;
+// alert(s);
+// alert(l);
+
+// begin_8 ikkita sonni orta arifmetigi 
+// const a = parseInt(prompt("Birinchi sonni kiriting"));
+// const b = parseInt(prompt("Ikkinchi sonni kiriting"));
+// const y = (a + b) / 2;
+// alert(2);
+
+// begin13 yuzalar ayirmasi
+// const r1 = parseInt(prompt("birnichi doiraning radiusini kiriting"))
+// const r2 = parseInt(prompt("ikkinchi doiraning radiusini kiriting"))
+// const pi = 3.14159265;
+// const s = pi * ( r1 - r2 )
+// alert(s)
+
+// begin_32 tempratura 
+// const t1 = parseInt(prompt("temptratura kiriting"))
+// const t2 = (t1 - 32) * 5 / 9
+// alert(t2)
+
+// Integer_1 
+// const L = parseInt(prompt("Kiritgan qiymatingiz santimetrda qabul qilinadi "))
+// const M = L / 100 
+// alert(M)
+
+// Integer_2
+// const M = parseInt(prompt("Kiritgan qiymatingiz santimetrda qabul qilinadi "))
+// const T = M / 1000
+// alert(M)
+
+// Integer_3
+// const B = parseInt(prompt("bayt hajmini kiriting"))
+// const K = B / 1024 
+// alert(K)
+
+// Integer_4 
+// const B = parseInt(prompt("Birinchi kesmani kiriting kiriting"))
+// const A = parseInt(prompt("Ikkinchi  kesmani kiriting kiriting"))
+// const L = B / A
+// alert(L)
+
+// Integer_5
+
+
+
+
+
+
